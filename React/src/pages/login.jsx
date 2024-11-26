25a19dfb7f5e860229be3357d9da3462223fff75
cassacogeek <cassacoloja@gmail.com>
2024-11-26 16:21:47 -0300

    login.jsx

--- React/src/pages/login.jsx
+++ React/src/pages/login.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Header from './header';
-import Footer from './footer';
 // import "../css/login.css";
 
 const Login = () => {
