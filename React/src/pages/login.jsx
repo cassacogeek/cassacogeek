429430645a5097de30f9c6c4cc32bae40092e7ca
cassacogeek <cassacoloja@gmail.com>
2024-11-26 20:56:33 -0300

    login.jsx

--- React/src/pages/login.jsx
+++ React/src/pages/login.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
 
-<div class="flex items-center justify-center h-screen bg-gray-50">
-  <div class="w-96 bg-gray-50 text-black border border-gray-500 rounded-lg p-5 shadow-lg text-center">
-    <h1 class="text-xl font-semibold mb-4">Escolha uma forma de login.</h1>
+<div className="flex items-center justify-center h-screen bg-gray-50">
+  <div className="w-96 bg-gray-50 text-black border border-gray-500 rounded-lg p-5 shadow-lg text-center">
+    <h1 className="text-xl font-semibold mb-4">Escolha uma forma de login.</h1>
     <Link to={"/entrar"}>
-      <button class="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
+      <button className="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
         Entrar
       </button>
     </Link>
     <Link to={"/cadastrar"}>
-      <button class="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
+      <button className="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
         Cadastrar
       </button>
     </Link>
