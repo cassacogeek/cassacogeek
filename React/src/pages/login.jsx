cfa5b530ec57f55a1a451133bed23dc1f74d28ae
cassacogeek <cassacoloja@gmail.com>
2024-11-26 20:02:56 -0300

    login.jsx

--- React/src/pages/login.jsx
+++ React/src/pages/login.jsx
@@ -8,16 +8,16 @@ const Login = () => {
 <div class="flex items-center justify-center h-screen bg-gray-50">
   <div class="w-96 bg-gray-50 text-black border border-gray-500 rounded-lg p-5 shadow-lg text-center">
     <h1 class="text-xl font-semibold mb-4">Escolha uma forma de login.</h1>
-    <a href="/entrar">
+    <Link to={"/entrar"}>
       <button class="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
         Entrar
       </button>
-    </a>
-    <a href="/cadastrar">
+    </Link>
+    <Link to={"/cadastrar"}>
       <button class="block w-3/4 mx-auto bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg hover:bg-gray-300 hover:text-gray-500 border transition-colors">
         Cadastrar
       </button>
-    </a>
+    </Link>
   </div>
 </div>
 
