7e11e35a7f35259b0832d208850dc861ae4f15c6
cassacogeek <cassacoloja@gmail.com>
2024-11-26 14:47:34 -0300

    login.jsx

--- React/src/pages/login.jsx
+++ React/src/pages/login.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Link } from "react-router-dom";
 
+import "../css/login.css";
+
+const Login = () => {
+    return (
+<div>
+        <div class="caixa">
+        <h1>Escolha uma forma de entrar.</h1>
+        <Link to="/entar">
+            <button class="button">Entrar</button>
+        </Link>
+        <Link to="/cadastrar">
+            <button class="button">Cadastrar</button>
+        </Link>
+        </div>
+    </div>
+    )
+};
+
+export default Login;
